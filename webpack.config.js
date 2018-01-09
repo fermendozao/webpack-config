6a8199419fe098f8e86df2253f5a29be76ef0101
Fernando Mendoza <goliat8903@gmail.com>
2018-01-09 14:48:35 -0600

    Adds autoprefixer via postcss

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ let config = {
       {
         test: /\.scss$/,
         use: ['css-hot-loader'].concat(ExtractTextWebpackPlugin.extract({
-          use: ['css-loader', 'sass-loader'],
+          use: ['css-loader', 'sass-loader', 'postcss-loader'],
           fallback: 'style-loader'
         })),
       },
