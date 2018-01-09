b83b9db4d23e46430842aa2221637f3506a4093a
Fernando Mendoza <goliat8903@gmail.com>
2018-01-09 14:35:26 -0600

    Adds images path alias

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
+import cat from 'images/cat.jpg';
+import dog from 'images/dog.png';
+import dogs from 'images/dogs.gif';
+
 
 const hello = () => {
   return (
-    <div>
-      <h1>Hello React</h1>
+    <div className="container">
+      <div className="image-wrapper">
+        <img src={cat} className="image" alt=""/>
+      </div>
+
+      <div className="image-wrapper">
+        <img src={dog} className="image" alt=""/>
+      </div>
+
+      <div className="image-wrapper">
+        <img src={dogs} className="image" alt=""/>
+      </div>
     </div>
   )
 }
